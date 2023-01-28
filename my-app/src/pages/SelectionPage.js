ec75b967da0af440f71be59fa5cadd5ede2fabdf
Justin Sun <jsunny246@gmail.com>
2023-01-28 15:33:35 -0800

    added therapistPage

---
+++ my-app/src/pages/SelectionPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SelectionPage = () => {
+    return (
+        <div>
+            <h1>Are you a therapist or looking for a therapist?</h1>
+            <button>I am a therapist</button>
+            <button>I am looking for a therapist</button>
+        </div>
+    );
+}
+
+export default SelectionPage;

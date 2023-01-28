aa15b932b616fd3b09a12cce13869fcd3b3f4d34
Justin Sun <jsunny246@gmail.com>
2023-01-28 15:26:43 -0800

    added a selection page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/Homepage';
+import SelectionPage from './pages/SelectionPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/selection" element={<SelectionPage />} />
       </Routes>
     </Router>
   );
--- my-app/src/pages/Homepage.js
+++ my-app/src/pages/Homepage.js
@@ -4,6 +4,7 @@ import therapy1 from '../therapy_pics/therapy1.jpeg';
 import therapy2 from '../therapy_pics/therapy2.jpeg';
 import therapy3 from '../therapy_pics/therapy3.jpeg';
 import therapy4 from '../therapy_pics/therapy4.jpeg';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
     return (
@@ -12,7 +13,9 @@ const HomePage = () => {
             <span className="BannerText">
                 <h1>Welcome to EmpathTherapy</h1>
                 <p>Find a therapist who understands you</p>
-                <button>Start Swiping</button>
+                <Link to="/selection">
+                  <button>Get Started</button>
+                </Link>
             </span>
             <img className="HeroImage" src={therapy1} alt="Therapy Session" />
             <img className="HeroImage" src={therapy2} alt="Therapy Session" />

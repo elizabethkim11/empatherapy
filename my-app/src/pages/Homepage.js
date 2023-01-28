c11cbff79ec8bb2819510969bdf19e3d70e7cb5a
marianne <tsayenn@gmail.com>
2023-01-28 15:14:26 -0800

    working hero banner

--- my-app/src/pages/Homepage.js
+++ my-app/src/pages/Homepage.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './Homepage.css';
+import therapy1 from '../therapy_pics/therapy1.jpeg';
+import therapy2 from '../therapy_pics/therapy2.jpeg';
+import therapy3 from '../therapy_pics/therapy3.jpeg';
+import therapy4 from '../therapy_pics/therapy4.jpeg';
 
 const HomePage = () => {
     return (
-        <div className="homepage">
-            <h1>Welcome to EmpathTherapy</h1>
-            <p>Find a therapist who understands you</p>
-            <button>Start Swiping</button>
+        <div className="Homepage">
+            <div className="Banner">
+            <span className="BannerText">
+                <h1>Welcome to EmpathTherapy</h1>
+                <p>Find a therapist who understands you</p>
+                <button>Start Swiping</button>
+            </span>
+            <img className="HeroImage" src={therapy1} alt="Therapy Session" />
+            <img className="HeroImage" src={therapy2} alt="Therapy Session" />
+            <img className="HeroImage" src={therapy3} alt="Therapy Session"/>
+            <img className="HeroImage" src={therapy4} alt="Therapy Session" />
+            </div>
         </div>
     );
 }
